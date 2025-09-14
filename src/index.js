b6ee3d0e9bdf9d9e58c26cd935478c845d166970
OgechaEnock <enockogecha7@gmail.com>
2025-09-14 17:09:34 +0300

    initialized DOM listerner and task arrays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  const sortPriorityAscBtn = document.querySelector("#sort-priority-asc");
+  const sortPriorityDescBtn = document.querySelector("#sort-priority-desc");
+  const sortDateAscBtn = document.querySelector("#sort-date-asc");
+  const sortDateDescBtn = document.querySelector("#sort-date-desc");
+
+  
